aa935c4caaf65251999c0d39ab976af3b93e34b5
Besufekd <besufekadsm@gmail.com>
2018-05-30 16:37:28 +0300

    add the request api

--- project.py
+++ project.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 
 #Request Responses
 login_successful={
-        'success':true
-        'message':"Logined in Succesfully."
+        'success':True,
+        'message':"Logined in Succesfully.",
         'token':123
         }
 login_fail={
-        'success':false
+        'success':False,
         'message':"Logined Failed."
+        }
+auth_fail={
+        'success':False,
+        'message':"You are not authorised to access this page."
         }
 requests = [{'id': 20003,'title': u'Range Rover','type': u'Repair','category': u'Cars','status':u'Completed'},{'id': 20004,'title': u'Samsung S7','type': u'Repair','category': u'Phones and Tablet','status':u'In Progress'}]
 
@@ -22,16 +26,16 @@ requests = [{'id': 20003,'title': u'Range Rover','type': u'Repair','category': u
 def api_documentation():
     return "Maintenance-Tracher"
 
-@app.route('/api/v1/login')
+@app.route('/api/v1/login', methods=['POST'])
 def api_login():
-    data = request.get_json(force==true)
-    username = data.get('name',None)
-    password = data.get('name',None)
+    data = request.args
+    username = data.get("username")
+    password = data.get("password")
 
     if username == "Bes":
         if password == "1234567890":
             return jsonify(login_successful)
-        else
+        else:
             return jsonify(login_fail)
     else:
         return jsonify(login_fail)
@@ -39,13 +43,19 @@ def api_login():
 
 @app.route('/api/v1/requests')
 def get_requests():
-    auth_token = request.headers.get('Authorization')
-    if auth_token:
-        response =UserLoginRequest.  
-    return jsonify(requests)
+    try:
+        token = request.headers["Authorization"]
+    except:
+        return jsonify(auth_fail)
+
+    if token == '123':
+        return jsonify(requests)
+    else:
+        return jsonify(auth_fail)
 
 @app.route('/requests', methods=['POST'])
 def add_request():
+
     requests.append(request.get_json())
     return '', 204
 
